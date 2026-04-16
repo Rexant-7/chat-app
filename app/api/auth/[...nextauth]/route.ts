d64da1b24abaa9233b3f23d4c5ba747af43f15f5
Rexant-7 <rexant.is.ren.ff0@gmail.com>
2026-04-16 14:24:02 +0900

    disable prisma for deploy

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,58 +11,67 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  callbacks: {
-    async signIn({ user }) {
-      // ① ユーザー作成 or 取得
-      const dbUser = await prisma.user.upsert({
-        where: { email: user.email! },
-        update: {},
-        create: {
-          email: user.email!,
-          name: user.name,
-          image: user.image,
-        },
-      });
+  // callbacks: {
+  // async signIn({ user }) {
+  //   // ① ユーザー作成 or 取得
+  //   const dbUser = await prisma.user.upsert({
+  //     where: { email: user.email! },
+  //     update: {},
+  //     create: {
+  //       email: user.email!,
+  //       name: user.name,
+  //       image: user.image,
+  //     },
+  //   });
 
-      // ② ルームに参加（ここ追加🔥）
-      await prisma.userRoom.upsert({
-        where: {
-          userId_roomId: {
-            userId: dbUser.id,
-            roomId: "room1", // とりあえず
-          },
-        },
-        update: {},
-        create: {
-          userId: dbUser.id,
-          roomId: "room1",
-        },
-      });
-      await prisma.userRoom.upsert({
-        where: {
-          userId_roomId: {
-            userId: dbUser.id,
-            roomId: "room2",
-          },
-        },
-        update: {},
-        create: {
-          userId: dbUser.id,
-          roomId: "room2",
-        },
-      });
+  //   // ② ルームに参加（ここ追加🔥）
+  //   await prisma.userRoom.upsert({
+  //     where: {
+  //       userId_roomId: {
+  //         userId: dbUser.id,
+  //         roomId: "room1", // とりあえず
+  //       },
+  //     },
+  //     update: {},
+  //     create: {
+  //       userId: dbUser.id,
+  //       roomId: "room1",
+  //     },
+  //   });
+  //   await prisma.userRoom.upsert({
+  //     where: {
+  //       userId_roomId: {
+  //         userId: dbUser.id,
+  //         roomId: "room2",
+  //       },
+  //     },
+  //     update: {},
+  //     create: {
+  //       userId: dbUser.id,
+  //       roomId: "room2",
+  //     },
+  //   });
+  //   return true;
+  // },
+
+  // async session({ session }) {
+  //   const dbUser = await prisma.user.findUnique({
+  //     where: { email: session.user.email! },
+  //   });
+
+  //   if (dbUser) {
+  //     session.user.id = dbUser.id; // ← 重要
+  //   }
+
+  //   return session;
+  // },
+  // },
+  callbacks: {
+    async signIn() {
       return true;
     },
 
     async session({ session }) {
-      const dbUser = await prisma.user.findUnique({
-        where: { email: session.user.email! },
-      });
-
-      if (dbUser) {
-        session.user.id = dbUser.id; // ← 重要
-      }
-
       return session;
     },
   },
