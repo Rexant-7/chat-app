ba90c96ef6e3697914603590dea441f0364644c2
Rexant-7 <rexant.is.ren.ff0@gmail.com>
2026-04-16 13:38:53 +0900

    remove unused socket files(socketの未使用ファイル削除)

--- app/api/_socket/_route.ts
+++ app/api/_socket/_route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { initSocket } from "../../lib/socket";
+// import { initSocket } from "../../../lib/socket";
 
 export async function GET() {
   // @ts-ignore
